7fbfe6dea6718f0ebd45fe8f4acdc226723dfe98
andydbui <buiand@fastmail.com>
2026-07-02 15:14:45 -0500

    scaffolding frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <section id="center">
+        <div className="hero">
+
+        </div>
+      </section>
+      <section id="spacer"></section>
+    </>
+  )
+}
+
+export default App
