a73801a8d268f5c7f1be188cc92e03c2bff04018
ADB4 <168307272+ADB4@users.noreply.github.com>
2026-08-01 14:36:09 -0500

    15 asset listing endpoint (#6)
    * creating AssetSpecification
    
    * fixing compose db

--- backend/src/main/java/com/adb4/rmlmanager/repository/AssetRepository.java
+++ backend/src/main/java/com/adb4/rmlmanager/repository/AssetRepository.java
@@ -5,13 +5,14 @@ import com.adb4.rmlmanager.enums.AssetStatus;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 import java.util.UUID;
 
-public interface AssetRepository extends JpaRepository<Asset, UUID> {
+public interface AssetRepository extends JpaRepository<Asset, UUID>, JpaSpecificationExecutor<Asset> {
     Optional<Asset> findByCode(String code);
     boolean existsByCode(String code);
 
